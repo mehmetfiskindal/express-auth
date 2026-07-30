831f049ea94d1d65054816a668d4133ee2ada6aa
Mehmet <developersailor@gmail.com>
2026-07-30 16:22:24 +0300

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // bcrypt hashing (cost 12) + a live HTTP round-trip can exceed Jest's
+  // default 5000ms on slower/older CI runners (observed on Node 18.x).
+  testTimeout: 15000,
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.spec.ts', '**/*.test.ts'],
   transform: {
